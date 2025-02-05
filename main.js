3aa63edd198a6771f65a084be80576c2e798232c
Mihsan Wakir <156558770+Mihsan17@users.noreply.github.com>
2025-02-05 08:03:59 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed= new Typed(".text",{
-    strings:["Frontend Developer","Freelancer","Web Developer"],
+    strings:["Engineering Student","IT Undergraduate","Web Developer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
